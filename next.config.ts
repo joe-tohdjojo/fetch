ed6dba46159ff86ac399dae694a9fe04ccc4bddd
joe <13431761+joseph-tohdjojo@users.noreply.github.com>
2025-02-07 06:01:45 -0500

    feat: Add VSCode settings for Prettier and ESLint configuration; update Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'frontend-take-home.fetch.com',
+        port: '',
+        pathname: '/dog-images/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
