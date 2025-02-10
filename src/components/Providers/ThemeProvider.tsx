bca0a5e64b8aa68c3253dfb32026a40be7841eb3
joe <13431761+joseph-tohdjojo@users.noreply.github.com>
2025-02-10 10:57:05 -0500

    feat: Add dropdown menu for theme selection and update ThemeProvider SSR setting

--- src/components/Providers/ThemeProvider.tsx
+++ src/components/Providers/ThemeProvider.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import * as React from 'react';
+// import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import dynamic from 'next/dynamic';
 const NextThemesProvider = dynamic(
   () => import('next-themes').then((e) => e.ThemeProvider),
   {
-    ssr: false,
+    ssr: true,
   },
 );
 
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,26 +5,39 @@ import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
-/**
- * Theme toggle component that toggles between light and dark mode
- * @returns {JSX.Element} Theme toggle component
- */
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      onClick={() => setTheme((theme) => (theme === 'dark' ? 'light' : 'dark'))}
-    >
-      {theme === 'dark' ? (
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-0 transition-all dark:-rotate-90 dark:scale-100" />
-      ) : (
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
-      )}
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+        >
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
